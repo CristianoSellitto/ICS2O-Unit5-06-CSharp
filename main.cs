42e85247a2a7d22d5ab2abe7052138eb02774be1
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-05-09 16:58:55 +0000

    Added program

--- main.cs
+++ main.cs
@@ -1,6 +1,6 @@
 // Cristiano
-// ICS2O-Unit0-00-CSharp
-// Date 2022
+// ICS2O-Unit5-06-CSharp
+// May 9 2022
 
 using System;
 
@@ -8,7 +8,45 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        int numberOne;
+        int numberTwo;
+        int timesRan;
+        int result = 0;
+      
+        Console.WriteLine("Enter the first number:");
+        while (!int.TryParse(Console.ReadLine(), out numberOne))
+        {
+            Console.WriteLine("Error: This isn't an integer.");
+            Environment.Exit(0);
+        }
+        Console.Clear();
+        Console.WriteLine("Enter the second number:");
+        while (!int.TryParse(Console.ReadLine(), out numberTwo))
+        {
+            Console.WriteLine("Error: This isn't an integer.");
+            Environment.Exit(0);
+        }
+        timesRan = numberTwo;
+        if (numberOne != 0 && numberTwo >= 0)
+        {
+            while (timesRan > 0)
+            {
+                // Calculation for + or - times + numbers
+                result = result + numberOne;
+                timesRan = timesRan - 1;
+            }
+        }
+        else if (numberTwo < 0)
+        {
+            while (timesRan != 0)
+            {
+                // Calculation for + or - times - numbers
+                result = result - numberOne;
+                timesRan = timesRan + 1;
+            }
+        }
+        Console.Clear();
+        Console.WriteLine("The result of " + numberOne + " × " + numberTwo + " is " + result);
         Console.WriteLine("\nFinished.");
     }
 }
